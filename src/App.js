dac7206afff90076637cf26b30e66aafc33cb480
anagparedes <anagparedesg@gmail.com>
2022-04-17 08:25:48 -0400

    [REF] using api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
-import data from "./data.json";
+import { getPeople } from './api/people';
 
 function App() {
+  const [people, setPeople] = useState([]);
+  
+  useEffect(() => {
+    getPeople().then((data) => setPeople(data.results));
+  },[])
+
   return (
     <ul>
-    {data.results.map((character) => (
+    {people.map((character) => (
       <li key={character.name}>{character.name}</li>
     ))}
       </ul>
